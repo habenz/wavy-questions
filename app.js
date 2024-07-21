b09b8fb4133b6f3e3861681b46dcf0305c8791ab
habenz <haben.zaidt@gmail.com>
2024-07-21 12:07:27 -0400

    make it wigglegit status

--- app.js
+++ app.js
@@ -19,15 +19,35 @@ ctx.fillText(toDisplay, width / 2, height / 2);
 document.body.append(textBox);
 
 // shift the image
-drawnText = ctx.getImageData(0, 0, width, height);
-let x = 0;
+const drawnText = ctx.getImageData(0, 0, width, height);
+let time = 0;
 setInterval(() => {
   ctx.clearRect(0, 0, width, height);
-  setFontAndStyling(ctx);
-  // .2*width is .333 of just the text width
-  ctx.putImageData(drawnText, -0.2 * width * Math.sin(toRadians(x)), 0);
-  x += 1;
-}, 10);
+  setFontAndStyling(ctx); // this line seems to be uneccessary
+
+  for (let y = 0; y < drawnText.height; y++) {
+    // grab the row of pixels
+    const row = new ImageData(drawnText.width, 1); //ImageData(width, height)
+    for (let x = 0; x < drawnText.width; x++) {
+      const redIndex = y * (4 * drawnText.width) + x * 4;
+      row.data[x * 4] = drawnText.data[redIndex];
+      row.data[x * 4 + 1] = drawnText.data[redIndex + 1];
+      row.data[x * 4 + 2] = drawnText.data[redIndex + 2];
+      row.data[x * 4 + 3] = drawnText.data[redIndex + 3];
+    }
+
+    // calulate the delay
+    const delay = y;
+    let locationToDrawX = 0;
+    if (time >= delay) {
+      // .2*width is .333 of just the text width
+      locationToDrawX = -0.2 * width * Math.sin(toRadians(time - delay));
+    }
+    ctx.putImageData(row, locationToDrawX, y);
+  }
+
+  time += 1;
+}, 5);
 
 function getCanvasSize(text) {
   const ctx = document.createElement("canvas").getContext("2d");
