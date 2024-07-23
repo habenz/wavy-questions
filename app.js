135dd784106ce0ac50c8e54ce0e48ec6df61dcb7
habenz <haben.zaidt@gmail.com>
2024-07-23 13:51:55 -0400

    allow viewer to set the wavy text

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import "./app.css";
 import { getWavyText } from "./wavyText";
+import controlsTemplate from "./controls.html";
 
 // TODO: probbaly move this into its own file or something
 new EventSource("/esbuild").addEventListener("change", () => location.reload());
@@ -9,3 +10,15 @@ const root = document.getElementById("wrapper");
 let textToDisplay = "???";
 let drawnText = getWavyText(textToDisplay);
 root.append(drawnText);
+
+const controls = document.createElement("div");
+controls.innerHTML = controlsTemplate;
+const button = controls.querySelector("#submitButton");
+
+button.addEventListener("click", () => {
+  textToDisplay = controls.querySelector("#textInput").value;
+  drawnText.remove();
+  drawnText = getWavyText(textToDisplay);
+  root.prepend(drawnText);
+});
+root.append(controls);
